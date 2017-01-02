d7327f4ed8ffd8db65f7137c616f98956453f82d
tenzin <ten13zin@gmail.com>
2017-01-02 14:57:09 +0530

    View instance Update

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 
 from .forms import PostForm
@@ -8,12 +8,8 @@ def post_create(request):
 	form = PostForm(request.POST)
 	if form.is_valid():
 		instance = form.save(commit=False)
-		print form.cleaned_data.get("title")
 		instance.save()
-
-	# if request.method == "POST":
-	# 	print request.POST.get("title")
-	# 	print request.POST.get("content")
+		return HttpResponseRedirect(instance.get.absolute_url())
 	context = {
 		"form": form,
 	}
@@ -33,20 +29,22 @@ def post_list(request): #list items
 		"object_lists": queryset,
 		"title": "List"
 	 }
-
-	# if request.user.is_authenticated():
-	# 	context = {
-	# 		"title": "My user List"
-	# 	}
-	# else:
-	# 	context = {
-	# 		"title": "List"
-	# 	}
 	return render(request, "index.html", context)
-	#return HttpResponse("<h1>List</h1>")
 
-def post_update(request):
-	return HttpResponse("<h1>Update</h1>")
+def post_update(request, id=None):
+	instance = get_object_or_404(Post, id=id)
+	form = PostForm(request.POST or None, instance=instance)
+	if form.is_valid():
+		instance = form.save(commit=False)
+		instance.save()
+		return HttpResponseRedirect(instance.get_absolute_url())
+
+	context = {
+		"title": instance.title,
+		"instance": instance,
+		"form": form,
+	}	
+	return render(request, "post_form.html", context)
 
 def post_delete(request):
 	return HttpResponse("<h1>Delete</h1>")
