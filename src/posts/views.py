fb82f41028937c7138136631e32a26dc23213056
tenzin <ten13zin@gmail.com>
2017-01-02 20:55:13 +0530

    Create Templates and use its Inheritance

--- src/posts/views.py
+++ src/posts/views.py
@@ -12,8 +12,7 @@ def post_create(request):
 		instance.save()
 		messages.success(request, "Successfully Created")
 		return HttpResponseRedirect(instance.get_absolute_url())
-	else:
-		messages.error(request, "Not Successfully Created")	
+		
 	context = {
 		"form": form,
 	}
@@ -33,7 +32,7 @@ def post_list(request): #list items
 		"object_lists": queryset,
 		"title": "List"
 	 }
-	return render(request, "index.html", context)
+	return render(request, "post_list.html", context)
 
 def post_update(request, id=None):
 	instance = get_object_or_404(Post, id=id)
