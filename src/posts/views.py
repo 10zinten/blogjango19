5c209d3eda367b0016937517c8e1f3217e8b067e
tenzin <ten13zin@gmail.com>
2017-01-03 10:58:34 +0530

    Add Pagination by Query set

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,9 +1,11 @@
 from django.contrib import messages
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .forms import PostForm
 from .models import Post
+
 
 def post_create(request):
 	form = PostForm(request.POST or None)
@@ -18,6 +20,7 @@ def post_create(request):
 	}
 	return render(request, "post_form.html", context)
 
+
 def post_detail(request, id=None): #retrieve
 	instance = get_b=get_object_or_404(Post, id=id)
 	context = {
@@ -26,13 +29,29 @@ def post_detail(request, id=None): #retrieve
 	}
 	return render(request, "post_detail.html", context)
 
+
 def post_list(request): #list items
-	queryset = Post.objects.all()
+	queryset_list = Post.objects.all() #.order_by('-timestamp')
+	paginator = Paginator(queryset_list, 3) # Show 25 contacts per page
+	page_request_var = "page"
+	page = request.GET.get(page_request_var)
+
+	try:
+		queryset = paginator.page(page)
+	except PageNotAnInteger:
+		# If page is not an integer, deliver first page.
+		queryset = paginator.page(1)
+	except EmptyPage:
+		# If page is out of range (e.g. 9999), deliver last page of results.
+		queryset = paginator.page(paginator.num_pages)
+
 	context = {
-		"object_lists": queryset,
-		"title": "List"
-	 }
+	"object_lists": queryset,
+	"title": "List",
+	"page_request_var": page_request_var
+	}
 	return render(request, "post_list.html", context)
+
 
 def post_update(request, id=None):
 	instance = get_object_or_404(Post, id=id)
@@ -49,6 +68,7 @@ def post_update(request, id=None):
 		"form": form,
 	}	
 	return render(request, "post_form.html", context)
+
 
 def post_delete(request, id=None):
 	instance = get_object_or_404(Post, id=id)
