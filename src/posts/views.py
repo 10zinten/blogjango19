dc64bb212be0dd6f8fc890455835cfe3a9e09994
tenzin <ten13zin@gmail.com>
2017-01-05 00:04:19 +0530

    File Uplaod with FileField and ImageField

--- src/posts/views.py
+++ src/posts/views.py
@@ -7,29 +7,6 @@ from .forms import PostForm
 from .models import Post
 
 
-def post_create(request):
-	form = PostForm(request.POST or None)
-	if form.is_valid():
-		instance = form.save(commit=False)
-		instance.save()
-		messages.success(request, "Successfully Created")
-		return HttpResponseRedirect(instance.get_absolute_url())
-		
-	context = {
-		"form": form,
-	}
-	return render(request, "post_form.html", context)
-
-
-def post_detail(request, id=None): #retrieve
-	instance = get_b=get_object_or_404(Post, id=id)
-	context = {
-		"title": instance.title,
-		"instance": instance
-	}
-	return render(request, "post_detail.html", context)
-
-
 def post_list(request): #list items
 	queryset_list = Post.objects.all() #.order_by('-timestamp')
 	paginator = Paginator(queryset_list, 3) # Show 25 contacts per page
@@ -53,9 +30,32 @@ def post_list(request): #list items
 	return render(request, "post_list.html", context)
 
 
+def post_create(request):
+	form = PostForm(request.POST or None, request.FILES or None)
+	if form.is_valid():
+		instance = form.save(commit=False)
+		instance.save()
+		messages.success(request, "Successfully Created")
+		return HttpResponseRedirect(instance.get_absolute_url())
+		
+	context = {
+		"form": form,
+	}
+	return render(request, "post_form.html", context)
+
+
+def post_detail(request, id=None): #retrieve
+	instance = get_object_or_404(Post, id=id)
+	context = {
+		"title": instance.title,
+		"instance": instance
+	}
+	return render(request, "post_detail.html", context)
+
+
 def post_update(request, id=None):
 	instance = get_object_or_404(Post, id=id)
-	form = PostForm(request.POST or None, instance=instance)
+	form = PostForm(request.POST or None, request.FILES or None, instance=instance)
 	if form.is_valid():
 		instance = form.save(commit=False)
 		instance.save()
