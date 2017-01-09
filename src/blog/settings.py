e3a02b18d05514b83d37f5b7fbff2962fbf88b81
tenzin <ten13zin@gmail.com>
2017-01-09 21:28:35 +0530

    Add django crispy form and implement pagedown

--- src/blog/settings.py
+++ src/blog/settings.py
@@ -37,10 +37,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # third party apps
+    'crispy_forms',
     'pagedown',
     'markdown_deux',
+
+    # local apps
     'posts',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
 
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
