3eae3f516b560b07230e2cf2a784b02028a533ac
tenzin <ten13zin@gmail.com>
2017-01-10 22:17:54 +0530

    Create comments

--- src/posts/models.py
+++ src/posts/models.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 
 from django.conf import settings
+from django.contrib.contenttypes.models import ContentType
 from django.core.urlresolvers import reverse
 from django.db import models
 from django.db.models.signals import pre_save
@@ -12,6 +13,7 @@ from markdown_deux import markdown
 import urllib
 
 from comments.models import Comment
+
 # Create your models here.
 # MVC MODEL VIEW CONTROLLER
 
@@ -71,6 +73,11 @@ class Post(models.Model):
 		qs = Comment.objects.filter_by_instance(instance)
 		return qs	
 
+	@property
+	def get_content_type(self):
+		instance = self
+		content_type = ContentType.objects.get_for_model(instance.__class__)
+		return content_type
 
 def create_slug(instance, new_slug=None):
     slug = slugify(instance.title)
--- src/posts/views.py
+++ src/posts/views.py
@@ -1,6 +1,7 @@
 from urllib import quote_plus
 
 from django.contrib import messages
+from django.contrib.contenttypes.models import ContentType
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render, get_object_or_404, redirect
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
@@ -8,6 +9,7 @@ from django.utils import timezone
 from django.db.models import Q
 
 from comments.models import Comment
+from comments.forms import CommentForm
 from .forms import PostForm
 from .models import Post
 
@@ -72,12 +74,32 @@ def post_detail(request, slug=None): #retrieve
 		if not request.user.is_staff or not request.user.is_superuser:
 			raise Http404
 	share_string = quote_plus(instance.content)
+
+	initial_data = {
+		"content_type": instance.get_content_type,
+		"object_id": instance.id,
+	}
+
+	form = CommentForm(request.POST or None, initial=initial_data)
+	if form.is_valid():
+		c_type = form.cleaned_data.get("content_type")
+		content_type = ContentType.objects.get(model=c_type)
+		obj_id = form.cleaned_data.get("object_id")
+		content_data = form.cleaned_data.get("content")
+		new_comment, created = Comment.objects.get_or_create(
+										user = request.user,
+										content_type = content_type,
+										object_id = obj_id,
+										content = content_data
+			)
+
 	comments = instance.comments #Comment.objects.filter_by_instance(instance)
 	context = {
 		"title": instance.title,
 		"instance": instance,
 		"share_string": share_string,
 		"comments": comments,
+		"comment_form": form,
 	}
 	return render(request, "post_detail.html", context)
 
